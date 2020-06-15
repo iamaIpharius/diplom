52e9433b30704efa19579bf468cbb632f10be904
Tabarkov Aleksey <ssturmmann@yandex.ru>
2020-06-15 11:21:46 +0300

    0.5 adopt 1

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 $(document).ready(function(){
-   
+   $(".owl-carousel").owlCarousel({
+   	loop: true,
+  	responsiveClass:true,
+  	responsiveRefreshRate: 15,
+  	responsive: {
+  		0: {
+  			dots: true,
+  			dotsEach: true,
+  			items: 1
+  		},
+  		576: {
+  			dots: true,
+  			dotsEach: true,
+  			items: 2
+  		},
+  		1024: {
+  			dots: false
+  		}
+  	}
+   });
+
   var owl = $(this).find('.owl-carousel').owlCarousel({
-  	loop: true,
-  	dots: false
+  	
   });
 
-  // При клике по кнопке Влево
+
   $(this).find('.left-arrow').on('click', function () {
-    // Перематываем карусель назад
+
     owl.trigger('prev.owl.carousel');
   });
 
-  // При клике по кнопке Вправо
+
   $(this).find('.right-arrow').on('click', function () {
-    // Перематываем карусель вперед
+
     owl.trigger('next.owl.carousel');
   });
 });
